093e63e08536d24017b8c3787367bfe2d2cacb87
Tyler Huynh <tyler02huy@gmail.com>
2025-10-21 22:36:13 -0700

    implemented fallback for build time (Vercel)

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 import { NextRequest, NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || '');
 
 export async function POST(request: NextRequest) {
   try {
