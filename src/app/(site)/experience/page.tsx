546ef84c0a1a8a1e3fef2a3afc0e0c68d7eecfb7
Tyler Huynh <tyler02huy@gmail.com>
2025-10-23 21:43:46 -0700

    implemented layout (containers) of exp page

--- src/app/(site)/experience/page.tsx
+++ src/app/(site)/experience/page.tsx
@@ -1,20 +1,247 @@
-import { FaTools } from "react-icons/fa";
-import { FaGear } from "react-icons/fa6";
 import { boxCx } from "@/lib/box";
 
 export default function ExperiencePage() {
   return (
     <div className={boxCx({ 
-      surface: "muted", 
-      direction: "row", 
-      paddingY: "lg",
+      surface: "transparent", 
+      // direction: "col", 
+      paddingY: "md",
+      // width: "full",
       items: "center", 
       justify: "start",
-      className: "my-[12rem] rounded-xl" 
+      className: "w-[72rem] h-[36rem] border-1" 
     })}>
-      <FaTools size={64} className="mx-[1rem]" />
-      <h1>page is currently under construction</h1>
-      <FaGear size={64} className="mx-[1rem]" />
+    {/* title */}
+    <div className={boxCx({ 
+      surface: "muted", 
+      // direction: "col",
+      paddingX: "lg",
+      paddingY: "md",
+      width: "full",
+      items: "start", 
+      justify: "start",
+      className: ""
+      })}>
+        <div className={boxCx({ 
+        surface: "transparent", 
+        // direction: "col",
+        paddingX: "md",
+        // paddingY: "xs",
+        width: "full",
+        items: "start", 
+        justify: "start",
+        className: "border-b-1 navbar-short-borders-bottom rounded-none text-title"
+        })}>
+          experience
+        </div>
     </div>
-    );
-  }
+
+      {/* client */}
+      <div className={boxCx({ 
+      surface: "muted", 
+      // direction: "col", 
+      // paddingY: "lg",
+      width: "full",
+      items: "center", 
+      justify: "start",
+      className: "border-1"
+      })}>
+        <div className={boxCx({ 
+      surface: "muted", 
+      direction: "row", 
+      // paddingY: "xl",
+      width: "full",
+      items: "center", 
+      justify: "start",
+      className: "border-1"
+        })}>
+          <div className={boxCx({ 
+            surface: "muted", 
+            // direction: "row", 
+            paddingY: "lg",
+            // width: "full",
+            items: "center", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            picture
+          </div>
+
+          <div className={boxCx({ 
+            surface: "muted", 
+            // direction: "row", 
+            paddingY: "lg",
+            width: "full",
+            items: "start", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            client software developer
+          </div>
+
+        </div>
+        <div className={boxCx({ 
+            surface: "muted", 
+            direction: "row", 
+            paddingY: "lg",
+            width: "full",
+            items: "center", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            techstack
+        </div>
+        <div className={boxCx({ 
+            surface: "muted", 
+            direction: "row", 
+            paddingY: "lg",
+            width: "full",
+            items: "center", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            description
+        </div>
+      </div>
+      
+      {/* open-source */}
+      <div className={boxCx({ 
+      surface: "muted", 
+      // direction: "col", 
+      // paddingY: "lg",
+      width: "full",
+      items: "center", 
+      justify: "start",
+      className: "border-1"
+      })}>
+        <div className={boxCx({ 
+      surface: "muted", 
+      direction: "row", 
+      // paddingY: "xl",
+      width: "full",
+      items: "center", 
+      justify: "start",
+      className: "border-1"
+        })}>
+
+          <div className={boxCx({ 
+            surface: "muted", 
+            // direction: "row", 
+            paddingY: "lg",
+            // width: "full",
+            items: "center", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            picture
+          </div>
+
+          <div className={boxCx({ 
+            surface: "muted", 
+            // direction: "row", 
+            paddingY: "lg",
+            width: "full",
+            items: "start", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            open-source software developer
+          </div>
+
+        </div>
+        <div className={boxCx({ 
+            surface: "muted", 
+            direction: "row", 
+            paddingY: "lg",
+            width: "full",
+            items: "center", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            techstack
+          </div>
+          <div className={boxCx({ 
+            surface: "muted", 
+            direction: "row", 
+            paddingY: "lg",
+            width: "full",
+            items: "center", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            description
+          </div>
+      </div>
+
+      {/* iet */}
+      <div className={boxCx({ 
+      surface: "muted", 
+      // direction: "col", 
+      // paddingY: "lg",
+      width: "full",
+      items: "center", 
+      justify: "start",
+      className: "border-1"
+      })}>
+        <div className={boxCx({ 
+      surface: "muted", 
+      direction: "row", 
+      // paddingY: "xl",
+      width: "full",
+      items: "center", 
+      justify: "start",
+      className: "border-1"
+        })}>
+
+          <div className={boxCx({ 
+            surface: "muted", 
+            // direction: "row", 
+            paddingY: "lg",
+            // width: "full",
+            items: "center", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            picture
+          </div>
+
+          <div className={boxCx({ 
+            surface: "muted", 
+            // direction: "row", 
+            paddingY: "lg",
+            width: "full",
+            items: "start", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            IET: jr. computer room consultant
+          </div>
+
+        </div>
+        <div className={boxCx({ 
+            surface: "muted", 
+            direction: "row", 
+            paddingY: "lg",
+            width: "full",
+            items: "center", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            techstack
+          </div>
+          <div className={boxCx({ 
+            surface: "muted", 
+            direction: "row", 
+            paddingY: "lg",
+            width: "full",
+            items: "center", 
+            justify: "start",
+            className: "border-1"
+          })}>
+            description
+          </div>
+      </div>
+
+    </div>
+  );
+}
